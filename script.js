199bdd1c67a03a69988e86835cf2fdaa3ec28f25
teran210c <you@example.com>
2024-11-20 17:26:19 -0700

    div flex

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 var board1 = Chessboard("board1", {
   position: "start",
   showNotation: false,
+  draggable: true
 });
 
 var board2 = Chessboard("board2", {
   position: "r1bqkbnr/pppp1ppp/2n5/1B2p3/4P3/5N2/PPPP1PPP/RNBQK2R",
   showNotation: false,
+  draggable: true
 });
 
 var board3 = Chessboard("board3", {
   position: "r1k4r/p2nb1p1/2b4p/1p1n1p2/2PP4/3Q1NB1/1P3PPP/R5K1",
   showNotation: false,
+  draggable: true
 });
