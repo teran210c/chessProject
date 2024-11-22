1cff1b0a1d3871587da92242ed864417e4a7ed46
teran210c <you@example.com>
2024-11-22 10:33:07 -0700

    stockfish.js

--- script.js
+++ script.js
@@ -1,14 +1,75 @@
-var board1 = Chessboard("board1", {
-  position: "start",
-  showNotation: false,
-});
+var game = new Chess();
+var board = null;
+var $status = $('#status');
+var $fen = $('#fen');
+var $pgn = $('#pgn');
 
-var board2 = Chessboard("board2", {
-  position: "r1bqkbnr/pppp1ppp/2n5/1B2p3/4P3/5N2/PPPP1PPP/RNBQK2R",
-  showNotation: false,
-});
+// Carga de Stockfish
+var stockfish = new Worker('js/stockfish.js');
 
-var board3 = Chessboard("board3", {
-  position: "r1k4r/p2nb1p1/2b4p/1p1n1p2/2PP4/3Q1NB1/1P3PPP/R5K1",
-  showNotation: false,
-});
+// Manejo de mensajes desde Stockfish
+stockfish.onmessage = function (e) {
+  console.log('Stockfish dice:', e.data);
+  if (e.data.startsWith('bestmove')) {
+    var bestMove = e.data.split(' ')[1];
+    game.move({ from: bestMove.slice(0, 2), to: bestMove.slice(2, 4), promotion: 'q' });
+    board.position(game.fen());
+    updateStatus();
+  }
+};
+
+function onDragStart(source, piece) {
+  if (game.game_over()) return false;
+  if ((game.turn() === 'w' && piece.startsWith('b')) || (game.turn() === 'b' && piece.startsWith('w'))) {
+    return false;
+  }
+}
+
+function onDrop(source, target) {
+  var move = game.move({
+    from: source,
+    to: target,
+    promotion: 'q'
+  });
+
+  if (move === null) return 'snapback';
+
+  updateStatus();
+
+  stockfish.postMessage('position fen ' + game.fen());
+  stockfish.postMessage('go depth 15');
+}
+
+function onSnapEnd() {
+  board.position(game.fen());
+}
+
+function updateStatus() {
+  var status = '';
+  var moveColor = game.turn() === 'b' ? 'Black' : 'White';
+
+  if (game.in_checkmate()) {
+    status = `Game over, ${moveColor} is in checkmate.`;
+  } else if (game.in_draw()) {
+    status = 'Game over, drawn position.';
+  } else {
+    status = `${moveColor} to move`;
+    if (game.in_check()) status += `, ${moveColor} is in check.`;
+  }
+
+  $status.html(status);
+  $fen.html(game.fen());
+  $pgn.html(game.pgn());
+}
+
+var config = {
+  draggable: true,
+  position: 'start',
+  onDragStart: onDragStart,
+  onDrop: onDrop,
+  onSnapEnd: onSnapEnd,
+  pieceTheme: 'img/chesspieces/wikipedia/{piece}.png'
+};
+
+board = Chessboard('board', config);
+updateStatus();
